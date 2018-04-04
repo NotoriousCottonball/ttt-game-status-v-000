cdeba189d76ae1b3eeb5baf4fee0cd43b6a31aeb
George  Pianka <gpianka1@gmail.com>
2018-04-04 08:28:43 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -32,17 +32,8 @@ def won?(board)
     end
   end
   false
+end
   
-  
-  
-  
-  def won?(board)
-    WIN_COMBINATIONS.detect do |win_combo|
-      board[win_combo[0]] == board[win_combo[1]] &&
-      board[win_combo[1]] == board[win_combo[2]] &&
-      position_taken?(board, win_combo[0])
-    end
-  end
   #Full Method
   def full?(board)
     board.all?{|index| index == "X" || index == "O"}
