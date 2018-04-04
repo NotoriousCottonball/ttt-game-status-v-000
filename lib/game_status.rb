363148a0b5ba86212cc4e22eb323ac94ecdeccae
George  Pianka <gpianka1@gmail.com>
2018-04-04 06:51:03 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,7 +28,7 @@ WIN_COMBINATIONS = [
       elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
         return win_combo
       else
-        false 
+        return false 
       end
     end
   end
